feaf74a2265c284ecd433062c5ef01f2542867f8
eduardotec05 <eduardo.dominguez7707@alumnos.udg.mx>
2025-01-30 08:18:06 -0600

    Add the funcionality

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigo = [];
+
+function agregarAmigo() {
+  let nombre = document.getElementById("amigo").value;
+  resultado.innerHTML = "";
+  document.getElementById("amigo").value = "";
+  if (nombre.trim() === "") {
+    alert("Debes ingresar un nombre");
+  } else {
+    amigo.push(nombre);
+    document.getElementById("amigo").value = "";
+  }
+  mostrarAmigos();
+}
+
+function mostrarAmigos() {
+  document.getElementById("listaAmigos").innerHTML = "";
+  for (let i = 0; i < amigo.length; i++) {
+    let li = document.createElement("li");
+    li.innerText = amigo[i];
+    document.getElementById("listaAmigos").appendChild(li);
+  }
+}
+
+function sortearAmigo() {
+  if (amigo.length === 0) {
+    alert("Debes ingresar al menos un amigo");
+    return;
+  }
+  indice = Math.floor(Math.random() * amigo.length);
+  let amigoSorteado = amigo[indice];
+  document.getElementById(
+    "resultado"
+  ).innerHTML = `El amigo secreto es ${amigoSorteado}`;
+  listaAmigos.innerHTML = "";
+  amigo.length = 0;
+}
